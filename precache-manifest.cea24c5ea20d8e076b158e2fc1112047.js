54629f2be0a5e5bc7eb804e6ba119fd5eb74daaf
Zipsoftstudio <zipsoftstudio@gmail.com>
2020-08-04 00:21:24 +0530

    Updates

---
+++ precache-manifest.cea24c5ea20d8e076b158e2fc1112047.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2782a4dd0a4e8cf5bfdac095238901e4",
+    "url": "/Todo-App/index.html"
+  },
+  {
+    "revision": "e123b202bb7c396b1ade",
+    "url": "/Todo-App/static/css/main.bf89f787.chunk.css"
+  },
+  {
+    "revision": "44ca37c1714396b130c4",
+    "url": "/Todo-App/static/js/2.4a833716.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Todo-App/static/js/2.4a833716.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e123b202bb7c396b1ade",
+    "url": "/Todo-App/static/js/main.0ff059ca.chunk.js"
+  },
+  {
+    "revision": "2ce1f4103cf2debb565c",
+    "url": "/Todo-App/static/js/runtime-main.c7ed4187.js"
+  }
+]);
